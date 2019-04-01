6152624739ae1ff28703eb2c819b11e0a229889f
Mikhail Barabanov <barabanov.mikle@gmail.com>
2019-04-01 15:31:11 +0300

    Face detector! And 14Kb instead of 5!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:share_extend/share_extend.dart';
 import 'package:image_picker/image_picker.dart';
@@ -10,6 +12,7 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:chewie/chewie.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:camera/camera.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,6 +29,79 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class FaceDetectorPainter extends CustomPainter {
+  FaceDetectorPainter(this.imageSize, this.faces);
+
+  final Size imageSize;
+  final List<Face> faces;
+
+  Rect _scaleRect({
+    @required Rect rect,
+    @required Size imageSize,
+    @required Size widgetSize,
+  }) {
+    final double scaleX = widgetSize.width / imageSize.width;
+    final double scaleY = widgetSize.height / imageSize.height;
+
+    return Rect.fromLTRB(
+      (imageSize.width - rect.left) * scaleX,
+      rect.top.toDouble() * scaleY,
+      (imageSize.width - rect.right) * scaleX,
+      rect.bottom.toDouble() * scaleY,
+    );
+  }
+
+  Offset _scaleOffset({
+    @required Offset offset,
+    @required Size imageSize,
+    @required Size widgetSize,
+  }) {
+    final double scaleX = widgetSize.width / imageSize.width;
+    final double scaleY = widgetSize.height / imageSize.height;
+
+    return Offset((imageSize.width - offset.dx) * scaleX, offset.dy * scaleY);//offset.scale(scaleX, scaleY);
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0
+      ..color = Colors.red;
+
+    for (Face face in faces) {
+//      face.getLandmark(Face)
+      for (var lm in FaceLandmarkType.values) {
+        var mark = face.getLandmark(lm);
+        if (mark != null)
+          canvas.drawCircle(
+              _scaleOffset(
+                  offset: mark.position,
+                  imageSize: imageSize,
+                  widgetSize: size),
+              10.0,
+              paint);
+      }
+
+      canvas.drawRect(
+        _scaleRect(
+          rect: face.boundingBox,
+          imageSize: imageSize,
+          widgetSize: size,
+        ),
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(FaceDetectorPainter oldDelegate) {
+    return oldDelegate.imageSize != imageSize || oldDelegate.faces != faces;
+  }
+}
+
+typedef HandleDetection = Future<dynamic> Function(FirebaseVisionImage image);
+
 class TakePhoto extends StatefulWidget {
   @override
   _TakePhotoState createState() => _TakePhotoState();
@@ -33,6 +109,8 @@ class TakePhoto extends StatefulWidget {
 
 class _TakePhotoState extends State<TakePhoto> {
   CameraController _camera;
+  bool _isDetecting = false;
+  dynamic _scanResults;
   CameraLensDirection _direction = CameraLensDirection.front;
 //  Detector _currentDetector = Detector.barcode;
 
@@ -50,40 +128,117 @@ class _TakePhotoState extends State<TakePhoto> {
     );
   }
 
+  Uint8List concatenatePlanes(List<Plane> planes) {
+    final WriteBuffer allBytes = WriteBuffer();
+    planes.forEach((Plane plane) => allBytes.putUint8List(plane.bytes));
+    return allBytes.done().buffer.asUint8List();
+  }
+
+  FirebaseVisionImageMetadata buildMetaData(
+    CameraImage image,
+    ImageRotation rotation,
+  ) {
+    return FirebaseVisionImageMetadata(
+      rawFormat: image.format.raw,
+      size: Size(image.width.toDouble(), image.height.toDouble()),
+      rotation: rotation,
+      planeData: image.planes.map(
+        (Plane plane) {
+          return FirebaseVisionImagePlaneMetadata(
+            bytesPerRow: plane.bytesPerRow,
+            height: plane.height,
+            width: plane.width,
+          );
+        },
+      ).toList(),
+    );
+  }
+
+  Future<dynamic> detect(
+    CameraImage image,
+    HandleDetection handleDetection,
+    ImageRotation rotation,
+  ) async {
+    return handleDetection(
+      FirebaseVisionImage.fromBytes(
+        concatenatePlanes(image.planes),
+        buildMetaData(image, rotation),
+      ),
+    );
+  }
+
+  ImageRotation rotationIntToImageRotation(int rotation) {
+    switch (rotation) {
+      case 0:
+        return ImageRotation.rotation0;
+      case 90:
+        return ImageRotation.rotation90;
+      case 180:
+        return ImageRotation.rotation180;
+      default:
+        assert(rotation == 270);
+        return ImageRotation.rotation270;
+    }
+  }
+
   void _initializeCamera() async {
     CameraDescription description = await getCamera(_direction);
-//    ImageRotation rotation = rotationIntToImageRotation(
-//      description.sensorOrientation,
-//    );
-    _camera = CameraController(description, ResolutionPreset.high
+    ImageRotation rotation = rotationIntToImageRotation(
+      description.sensorOrientation,
+    );
+    _camera = CameraController(description, ResolutionPreset.low
 //      defaultTargetPlatform == TargetPlatform.iOS
 //          ? ResolutionPreset.low
 //          : ResolutionPreset.medium,
         );
     await _camera.initialize();
-    setState(() {
+    setState(() {});
 
+    final FirebaseVision mlVision = FirebaseVision.instance;
+    _camera.startImageStream((CameraImage image) {
+      if (_isDetecting) return;
+
+      _isDetecting = true;
+      var detector = mlVision.faceDetector(FaceDetectorOptions(
+          enableLandmarks: true, mode: FaceDetectorMode.accurate));
+      detect(image, detector.processImage, rotation).then(
+        (dynamic result) {
+          setState(() {
+            _scanResults = result;
+          });
+
+          _isDetecting = false;
+        },
+      ).catchError(
+        (_) {
+          _isDetecting = false;
+        },
+      );
     });
+  }
 
-//    _camera.startImageStream((CameraImage image) {
-//      if (_isDetecting) return;
-//
-//      _isDetecting = true;
-//
-//      detect(image, _getDetectionMethod(), rotation).then(
-//            (dynamic result) {
-//          setState(() {
-//            _scanResults = result;
-//          });
-//
-//          _isDetecting = false;
-//        },
-//      ).catchError(
-//            (_) {
-//          _isDetecting = false;
-//        },
-//      );
-//    });
+  Widget _buildResults() {
+    const Text noResultsText = const Text('No results!');
+
+    if (_scanResults == null ||
+        _camera == null ||
+        !_camera.value.isInitialized) {
+      return noResultsText;
+    }
+
+    CustomPainter painter;
+
+    final Size imageSize = Size(
+      _camera.value.previewSize.height,
+      _camera.value.previewSize.width,
+    );
+
+    if (_scanResults is! List<Face>) return noResultsText;
+    painter = FaceDetectorPainter(imageSize, _scanResults);
+
+    return CustomPaint(
+      painter: painter,
+    );
   }
 
   Widget _buildImage() {
@@ -99,7 +254,7 @@ class _TakePhotoState extends State<TakePhoto> {
               fit: StackFit.expand,
               children: <Widget>[
                 CameraPreview(_camera),
-//          _buildResults(),
+                _buildResults(),
               ],
             ),
     );
