935e2b30c5470be177ea55c9d6bea85b072377de
Mikhail Barabanov <barabanov.mikle@gmail.com>
2019-04-06 00:01:03 +0300

    var rename

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
 import'dart:io';import'package:flutter/services.dart';import'package:flutter/foundation.dart';import'package:flutter/material.dart';import'package:share_extend/share_extend.dart';import'package:path_provider/path_provider.dart';import'package:flutter_ffmpeg/flutter_ffmpeg.dart';import'package:permission_handler/permission_handler.dart';import'package:camera/camera.dart';import'package:firebase_ml_vision/firebase_ml_vision.dart';import'package:video_player/video_player.dart';import'package:chewie/chewie.dart';import'package:flutter_scroll_gallery/flutter_scroll_gallery.dart';
-var F=false,T=true,X='FaceTimelapse',Dd,Pd,Z=0,L,Fc;
+var F=false,T=true,X='FaceTimelapse',D,P,Z=0,L,Fc;
 B(i,p)=>IconButton(icon:Icon(i),onPressed:p);
 CPI()=>Center(child:CircularProgressIndicator());
 main(){SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);runApp(A());}
 class A extends StatelessWidget{build(c)=>MaterialApp(home:HP(),title:X,theme:ThemeData.dark());}
 class FDP extends CustomPainter{
-  FDP(this.S,this.O,this.C);var S,O,C;
-  paint(c,s){
-    if(S==null)return;
-    var p=Paint()..color=C..style=PaintingStyle.stroke..strokeWidth=5,x=s.width/S.width,y=s.height/S.height;
+  FDP(this.d,this.f,this.c);var d,f,c;
+  paint(k,z){
+    if(d==null)return;
+    var p=Paint()..color=c..style=PaintingStyle.stroke..strokeWidth=5,x=z.width/d.width,y=z.height/d.height;
 
-    for(var o in O){
+    for(var i in f){
       for(var l in FaceLandmarkType.values){
-        var m=o.getLandmark(l);
+        var m=i.getLandmark(l);
         if(m!=null)
-          c.drawCircle(Offset((S.width-m.position.dx)*x,m.position.dy*y),10,p);
+          k.drawCircle(Offset((d.width-m.position.dx)*x,m.position.dy*y),10,p);
       }
-      var q=S.height*y,r=Rect.fromLTRB(0,q*0.1,S.width*x,q*0.9);
-      c.drawArc(r,1.57,o.headEulerAngleY/57.29,F,p);
-      c.drawArc(r,4.71,o.headEulerAngleZ/57.29,F,p);
+      var q=d.height*y,r=Rect.fromLTRB(0,q*0.1,d.width*x,q*0.9);
+      k.drawArc(r,1.57,i.headEulerAngleY/57.29,F,p);
+      k.drawArc(r,4.71,i.headEulerAngleZ/57.29,F,p);
     }
   }
   shouldRepaint(o)=>o!=this;
@@ -26,7 +26,7 @@ class FDP extends CustomPainter{
 
 class TP extends StatefulWidget{createState()=>TPS();}
 class TPS extends State<TP>{
-  var C,d=F,q=[],o=[],s;
+  var c,d=F,q=[],o=[],s;
   initState(){super.initState();iC();}
   iO()async{
     if(L==null)return;
@@ -45,9 +45,9 @@ class TPS extends State<TP>{
     await iO();
     var a=await availableCameras();
     if(Z==a.length)Z=0;
-    C=CameraController(a[Z],ResolutionPreset.medium);
-    await C.initialize();
-    C.startImageStream((CameraImage i)async{
+    c=CameraController(a[Z],ResolutionPreset.medium);
+    await c.initialize();
+    c.startImageStream((CameraImage i)async{
       if(d)return;
       d=T;
       try{
@@ -70,27 +70,27 @@ class TPS extends State<TP>{
       }finally{d=F;}
     });
   }
-  build(c)=>Scaffold(
+  build(k)=>Scaffold(
       floatingActionButton:FloatingActionButton(
         onPressed:()async{
-          await C.stopImageStream();
+          await c.stopImageStream();
           await Future.delayed(Duration(seconds:1));
-          var p='$Pd/${Fc.toString().padLeft(5,'0')}.jpg';
-          await C.takePicture(p);
+          var p='$P/${Fc.toString().padLeft(5,'0')}.jpg';
+          await c.takePicture(p);
           await FlutterFFmpeg().execute('-y -i $p -vf scale=1280:-2 $p');
-          Navigator.of(c).pop();
+          Navigator.of(k).pop();
         },backgroundColor:Colors.white,
         child:Icon(Icons.camera)
       ),
-      appBar:AppBar(actions:[B(Icons.switch_camera,()async{Z++;await C.dispose();setState((){C=null;});iC();})]),
+      appBar:AppBar(actions:[B(Icons.switch_camera,()async{Z++;await c.dispose();setState((){c=null;});iC();})]),
       body:Container(
           constraints:BoxConstraints.expand(),
-          child:C?.value?.isInitialized??F?Stack(
+          child:c?.value?.isInitialized??F?Stack(
                   fit:StackFit.expand,
                   children:[
-                    CameraPreview(C),
+                    CameraPreview(c),
                     CustomPaint(painter:FDP(s,o,Colors.red)),
-                    CustomPaint(painter:FDP(C.value.previewSize.flipped,q,Colors.green))
+                    CustomPaint(painter:FDP(c.value.previewSize.flipped,q,Colors.green))
                   ]
                 ):CPI()));
 }
@@ -98,19 +98,18 @@ class TPS extends State<TP>{
 class HP extends StatefulWidget{createState()=>HPS();}
 class VP extends StatefulWidget{createState()=>VPS();}
 class VPS extends State<VP>{
-  var i=T,m='$Dd/v.mp4',v,w;
+  var i=T,m='$D/v.mp4',v,w;
   initState(){super.initState();K();}
   dispose(){v?.dispose();w?.dispose();super.dispose();}
   K()async{
     var f=FlutterFFmpeg();
-    await f.execute('-y -r 1 -i $Pd/%05d.jpg -c:v libx264 $m');
-    var g=await f.getMediaInformation(m);
-    var s=g['streams'][0];
+    await f.execute('-y -r 1 -i $P/%05d.jpg -c:v libx264 $m');
+    var g=await f.getMediaInformation(m),s=g['streams'][0];
     setState((){i=F;});
     v=VideoPlayerController.file(File(m));
     w=ChewieController(videoPlayerController:v,autoPlay:T,looping:T,aspectRatio:s['width']/s['height']);
   }
-  build(c)=>Scaffold(body:i?CPI():Center(child:Chewie(controller:w)),appBar:AppBar(actions:i?[]:[B(Icons.share,(){ShareExtend.share(m,"video");})]));
+  build(k)=>Scaffold(body:i?CPI():Center(child:Chewie(controller:w)),appBar:AppBar(actions:i?[]:[B(Icons.share,(){ShareExtend.share(m,"video");})]));
 }
 
 class HPS extends State<HP>{
@@ -122,21 +121,21 @@ class HPS extends State<HP>{
       await SystemChannels.platform.invokeMethod('SystemNavigator.pop');
       return;
     }
-    Dd=(await Directory('${(await getExternalStorageDirectory()).path}/$X').create()).path;
-    var d=await Directory('$Dd/p').create();
+    D=(await Directory('${(await getExternalStorageDirectory()).path}/$X').create()).path;
+    var d=await Directory('$D/p').create();
     i=d.listSync().map((e)=>File(e.path)).toList();
-    Pd=d.path;
+    P=d.path;
     Fc=i.length;
     if(Fc>0)L=i.last;
     setState((){});
   }
-  M(c,w)=>Navigator.of(c).push(MaterialPageRoute(builder:(_)=>w));
-  build(c)=>Scaffold(
+  M(k,w)=>Navigator.of(k).push(MaterialPageRoute(builder:(_)=>w));
+  build(k)=>Scaffold(
       appBar:AppBar(
         title:Text(X),
         actions:[
-          B(Icons.movie_creation,()=>M(c,VP())),
-          B(Icons.add_a_photo,()=>M(c,TP()).then((_)=>iI())),
+          B(Icons.movie_creation,()=>M(k,VP())),
+          B(Icons.add_a_photo,()=>M(k,TP()).then((_)=>iI())),
         ]
       ),
       body:ScrollGallery(i.map((s)=>Image.file(s).image).toList().reversed.toList(),fit:BoxFit.cover,borderColor:Colors.white)
