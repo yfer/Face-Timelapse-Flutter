2c92defc7aee7ceea1bd733f6d70cafe6923f5c3
Mikhail Barabanov <barabanov.mikle@gmail.com>
2019-04-06 02:24:51 +0300

    5118

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import'dart:io';import'package:flutter/services.dart';import'package:flutter/foundation.dart';import'package:flutter/material.dart';import'package:share_extend/share_extend.dart';import'package:path_provider/path_provider.dart';import'package:flutter_ffmpeg/flutter_ffmpeg.dart';import'package:permission_handler/permission_handler.dart';import'package:camera/camera.dart';import'package:firebase_ml_vision/firebase_ml_vision.dart';import'package:video_player/video_player.dart';import'package:chewie/chewie.dart';import'package:flutter_scroll_gallery/flutter_scroll_gallery.dart';
-var F=false,T=true,X='FaceTimelapse',D,P,Z=0,L,H;
+var F=false,T=true,X='FaceTimelapse',D,P,Z=0,L,H,G=PermissionGroup.values,W=Colors.white;
 B(i,p)=>IconButton(icon:Icon(i),onPressed:p);
 main(){SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);runApp(A());}
 class A extends StatelessWidget{build(c)=>MaterialApp(home:HP(),title:X,theme:ThemeData.dark());}
 class FP extends CustomPainter{
   FP(this.d,this.f,this.c);var d,f,c;
   paint(k,z){
-    if(d==null)return;
-    var p=Paint()..color=c..style=PaintingStyle.stroke..strokeWidth=5,x=z.width/d.width,y=z.height/d.height;
-
-    for(var i in f){
-      for(var l in FaceLandmarkType.values){
-        var m=i.getLandmark(l);
-        if(m!=null)
-          k.drawCircle(Offset((d.width-m.position.dx)*x,m.position.dy*y),10,p);
+    if(d!=null){
+      var p=Paint()..color=c..style=PaintingStyle.stroke..strokeWidth=5,x=z.width/d.width,y=z.height/d.height;
+      for(var i in f){
+        for(var l in FaceLandmarkType.values){
+          var m=i.getLandmark(l);
+          if(m!=null)
+            k.drawCircle(Offset((d.width-m.position.dx)*x,m.position.dy*y),10,p);
+        }
+        var q=d.height*y,r=Rect.fromLTRB(0,q*0.1,d.width*x,q*0.9);
+        k.drawArc(r,1.57,i.headEulerAngleY/57.29,F,p);
+        k.drawArc(r,4.71,i.headEulerAngleZ/57.29,F,p);
       }
-      var q=d.height*y,r=Rect.fromLTRB(0,q*0.1,d.width*x,q*0.9);
-      k.drawArc(r,1.57,i.headEulerAngleY/57.29,F,p);
-      k.drawArc(r,4.71,i.headEulerAngleZ/57.29,F,p);
     }
   }
   shouldRepaint(o)=>o!=this;
@@ -25,20 +25,19 @@ class FP extends CustomPainter{
 
 class TP extends StatefulWidget{createState()=>TPS();}
 class TPS extends State<TP>{
-  var c,d=F,q=[],o=[],s;
+  var c,d=F,q=[],o=[],s,V=FirebaseVision.instance.faceDetector(FaceDetectorOptions(enableLandmarks:T,mode:FaceDetectorMode.accurate)).processImage;
   initState(){super.initState();iC();}
   iO()async{
-    if(L==null)return;
-    var v=FirebaseVisionImage.fromFilePath(L.path),f=Image.file(File.fromUri(L.uri));
-    f.image.resolve(ImageConfiguration()).completer.addListener((i,b)async{
-      o=await V(v);
-      setState((){
-        s=Size(i.image.width.toDouble(),i.image.height.toDouble());
+    if(L!=null){
+      var v=FirebaseVisionImage.fromFilePath(L.path),f=Image.file(File.fromUri(L.uri));
+      f.image.resolve(ImageConfiguration()).completer.addListener((i,b)async{
+        o=await V(v);
+        setState((){
+          s=Size(i.image.width.toDouble(),i.image.height.toDouble());
+        });
       });
-    });
+    }
   }
-
-  var V=FirebaseVision.instance.faceDetector(FaceDetectorOptions(enableLandmarks:T,mode:FaceDetectorMode.accurate)).processImage;
 
   iC()async{
     await iO();
@@ -47,26 +46,27 @@ class TPS extends State<TP>{
     c=CameraController(a[Z],ResolutionPreset.medium);
     await c.initialize();
     c.startImageStream((CameraImage i)async{
-      if(d)return;
-      d=T;
-      try{
-        var b=WriteBuffer();
-        i.planes.forEach((p)=>b.putUint8List(p.bytes));
-        var v=FirebaseVisionImageMetadata(
-          rawFormat:i.format.raw,
-          size:Size(i.width.toDouble(),i.height.toDouble()),
-          rotation:ImageRotation.rotation270,
-          planeData:i.planes
-              .map((p)=>FirebaseVisionImagePlaneMetadata(
-                    bytesPerRow:p.bytesPerRow,
-                    height:p.height,
-                    width:p.width
-                  ))
-              .toList()
-        );
-        q=await V(FirebaseVisionImage.fromBytes(b.done().buffer.asUint8List(),v));
-        if(mounted)setState((){});
-      }finally{d=F;}
+      if(!d){
+        d=T;
+        try{
+          var b=WriteBuffer();
+          i.planes.forEach((p)=>b.putUint8List(p.bytes));
+          var v=FirebaseVisionImageMetadata(
+            rawFormat:i.format.raw,
+            size:Size(i.width.toDouble(),i.height.toDouble()),
+            rotation:ImageRotation.rotation270,
+            planeData:i.planes
+                .map((p)=>FirebaseVisionImagePlaneMetadata(
+                      bytesPerRow:p.bytesPerRow,
+                      height:p.height,
+                      width:p.width
+                    ))
+                .toList()
+          );
+          q=await V(FirebaseVisionImage.fromBytes(b.done().buffer.asUint8List(),v));
+          if(mounted)setState((){});
+        }finally{d=F;}
+      }
     });
   }
   build(k)=>Scaffold(
@@ -77,7 +77,7 @@ class TPS extends State<TP>{
           await c.takePicture(p);
           await FlutterFFmpeg().execute('-y -i $p -vf scale=1280:-2 $p');
           Navigator.of(k).pop();
-        },backgroundColor:Colors.white,
+        },backgroundColor:W,
         child:Icon(Icons.camera)
       ),
       appBar:AppBar(actions:[B(Icons.sync,()async{Z++;await c.dispose();setState((){c=null;});iC();})]),
@@ -115,8 +115,8 @@ class HPS extends State<HP>{
   var i=[];
   initState(){super.initState();iI();}
   iI()async{
-    if((await PermissionHandler().requestPermissions([PermissionGroup.storage,PermissionGroup.camera,PermissionGroup.microphone])).values.where((p)=>p==PermissionStatus.granted).length!=3){
-      await showDialog(context:context, builder:(b)=>AlertDialog(title:Text('App want theese rights')));
+    if((await PermissionHandler().requestPermissions([G[2],G[5],G[11]])).values.where((p)=>p.index==2).length!=3){
+      await showDialog(context:context,builder:(b)=>AlertDialog(title:Text('App want theese rights')));
       await SystemChannels.platform.invokeMethod('SystemNavigator.pop');
       return;
     }
@@ -133,10 +133,10 @@ class HPS extends State<HP>{
       appBar:AppBar(
         title:Text(X),
         actions:[
-          B(Icons.movie_creation,()=>M(k,VP())),
+          B(Icons.movie,()=>M(k,VP())),
           B(Icons.add_a_photo,()=>M(k,TP()).then((_)=>iI()))
         ]
       ),
-      body:ScrollGallery(i.map((s)=>Image.file(s).image).toList().reversed.toList(),fit:BoxFit.cover,borderColor:Colors.white)
+      body:ScrollGallery(i.map((s)=>Image.file(s).image).toList().reversed.toList(),fit:BoxFit.cover,borderColor:W)
     );
 }
