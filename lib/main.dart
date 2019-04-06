d41ee9679ad53a62946a2f888d489e7b6f4d8b77
Mikhail Barabanov <barabanov.mikle@gmail.com>
2019-04-06 09:18:23 +0300

    added some instructions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import'dart:io';import'package:flutter/services.dart';import'package:flutter/foundation.dart';import'package:flutter/material.dart';import'package:share_extend/share_extend.dart';import'package:path_provider/path_provider.dart';import'package:flutter_ffmpeg/flutter_ffmpeg.dart';import'package:permission_handler/permission_handler.dart';import'package:camera/camera.dart';import'package:firebase_ml_vision/firebase_ml_vision.dart';import'package:video_player/video_player.dart';import'package:chewie/chewie.dart';import'package:flutter_scroll_gallery/flutter_scroll_gallery.dart';
-var F=false,T=true,X='FaceTimelapse',D,P,Z=0,L,H,G=PermissionGroup.values,W=Colors.white;
+var F=false,T=true,X='FaceTimelapse',D,P,Y=0,L,H=0,G=PermissionGroup.values,W=Colors.white;
 B(i,p)=>IconButton(icon:Icon(i),onPressed:p);
+U(i,p)=>FloatingActionButton(child:Icon(Icons.camera),onPressed:p);
+S(t,b,[List<Widget> a,u])=>Scaffold(appBar:AppBar(title:Text(t),actions:a),body:b,floatingActionButton:u);
+C(v)=>Center(child:v);
+Z(i)=>Size(i.width*1.0,i.height*1.0);
 main(){SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);runApp(A());}
 class A extends StatelessWidget{build(c)=>MaterialApp(home:HP(),title:X,theme:ThemeData.dark());}
 class FP extends CustomPainter{
@@ -33,7 +37,7 @@ class TPS extends State<TP>{
       f.image.resolve(ImageConfiguration()).completer.addListener((i,b)async{
         o=await V(v);
         setState((){
-          s=Size(i.image.width.toDouble(),i.image.height.toDouble());
+          s=Z(i.image);
         });
       });
     }
@@ -42,8 +46,8 @@ class TPS extends State<TP>{
   iC()async{
     await iO();
     var a=await availableCameras();
-    if(Z==a.length)Z=0;
-    c=CameraController(a[Z],ResolutionPreset.medium);
+    if(Y==a.length)Y=0;
+    c=CameraController(a[Y],ResolutionPreset.medium);
     await c.initialize();
     c.startImageStream((CameraImage i)async{
       if(!d){
@@ -53,7 +57,7 @@ class TPS extends State<TP>{
           i.planes.forEach((p)=>b.putUint8List(p.bytes));
           var v=FirebaseVisionImageMetadata(
             rawFormat:i.format.raw,
-            size:Size(i.width.toDouble(),i.height.toDouble()),
+            size:Z(i),
             rotation:ImageRotation.rotation270,
             planeData:i.planes
                 .map((p)=>FirebaseVisionImagePlaneMetadata(
@@ -69,31 +73,28 @@ class TPS extends State<TP>{
       }
     });
   }
-  build(k)=>Scaffold(
-      floatingActionButton:FloatingActionButton(
-        onPressed:()async{
-          await c.stopImageStream();
-          var p='$P/${H.toString().padLeft(5,'0')}.jpg';
-          await c.takePicture(p);
-          await FlutterFFmpeg().execute('-y -i $p -vf scale=1280:-2 $p');
-          Navigator.of(k).pop();
-        },backgroundColor:W,
-        child:Icon(Icons.camera)
-      ),
-      appBar:AppBar(actions:[B(Icons.sync,()async{Z++;await c.dispose();setState((){c=null;});iC();})]),
-      body:Container(
-          constraints:BoxConstraints.expand(),
-          child:c?.value?.isInitialized??F?Stack(
-                  fit:StackFit.expand,
-                  children:[
-                    CameraPreview(c),
-                    CustomPaint(painter:FP(s,o,Colors.red)),
-                    CustomPaint(painter:FP(c.value.previewSize.flipped,q,Colors.green))
-                  ]
-                ):Center(child:CircularProgressIndicator())));
+  build(k)=>S(
+      'Camera',
+      Container(
+        constraints:BoxConstraints.expand(),
+        child:c?.value?.isInitialized??F?Stack(
+            fit:StackFit.expand,
+            children:[
+              CameraPreview(c),
+              CustomPaint(painter:FP(s,o,Colors.red)),
+              CustomPaint(painter:FP(c.value.previewSize.flipped,q,Colors.green))
+            ]
+        ):C(CircularProgressIndicator())),
+      [B(Icons.sync,()async{Y++;await c.dispose();setState((){c=null;});iC();})],
+      U(Icons.camera,()async{
+        await c.stopImageStream();
+        var p='$P/${H.toString().padLeft(5,'0')}.jpg';
+        await c.takePicture(p);
+        await FlutterFFmpeg().execute('-y -i $p -vf scale=1280:-2 $p');
+        Navigator.of(k).pop();
+      })
+  );
 }
-
-class HP extends StatefulWidget{createState()=>HPS();}
 class VP extends StatefulWidget{createState()=>VPS();}
 class VPS extends State<VP>{
   var i=T,m='$D/v.mp4',v,w,p=0;
@@ -108,15 +109,19 @@ class VPS extends State<VP>{
     w=ChewieController(videoPlayerController:v,autoPlay:T,looping:T,aspectRatio:s['width']/s['height']);
     setState((){i=F;});
   }
-  build(k)=>Scaffold(body:Center(child:i?Text('${(p*10/H).round()}%'):Chewie(controller:w)),appBar:AppBar(actions:i?[]:[B(Icons.share,(){ShareExtend.share(m,"video");})]));
+  build(k)=>S(
+    'Movie',
+    C(i?Text('${(p*10/H).round()}%'):Chewie(controller:w)),
+    i?[]:[B(Icons.share,(){ShareExtend.share(m,"video");})]
+  );
 }
-
+class HP extends StatefulWidget{createState()=>HPS();}
 class HPS extends State<HP>{
   var i=[];
   initState(){super.initState();iI();}
   iI()async{
     if((await PermissionHandler().requestPermissions([G[2],G[5],G[11]])).values.where((p)=>p.index==2).length!=3){
-      await showDialog(context:context,builder:(b)=>AlertDialog(title:Text('App want theese rights')));
+      await showDialog(context:context,builder:(b)=>AlertDialog(title:Text('App need rights')));
       await SystemChannels.platform.invokeMethod('SystemNavigator.pop');
       return;
     }
@@ -128,15 +133,10 @@ class HPS extends State<HP>{
     if(H>0)L=i.last;
     setState((){});
   }
-  M(k,w)=>Navigator.of(k).push(MaterialPageRoute(builder:(_)=>w));
-  build(k)=>Scaffold(
-      appBar:AppBar(
-        title:Text(X),
-        actions:[
-          B(Icons.movie,()=>M(k,VP())),
-          B(Icons.add_a_photo,()=>M(k,TP()).then((_)=>iI()))
-        ]
-      ),
-      body:ScrollGallery(i.map((s)=>Image.file(s).image).toList().reversed.toList(),fit:BoxFit.cover,borderColor:W)
-    );
+  N(k,w)=>Navigator.of(k).push(MaterialPageRoute(builder:(_)=>w));
+  build(k)=>S(
+    X,
+    H>0?ScrollGallery(i.map((s)=>Image.file(s).image).toList().reversed.toList(),fit:BoxFit.cover,borderColor:W):C(Text('Take a photo')),
+    H>0?[B(Icons.movie,()=>N(k,VP()))]:[]..add(B(Icons.add_a_photo,()=>N(k,TP()).then((_)=>iI())))
+  );
 }
