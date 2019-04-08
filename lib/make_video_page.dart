112459ec48dfb83d05276cc6dd34459138c3fea8
Mikhail Barabanov <barabanov.mikle@gmail.com>
2019-04-08 19:45:40 +0300

    Added support for video complation cancellation

--- lib/make_video_page.dart
+++ lib/make_video_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:chewie/chewie.dart';
@@ -20,14 +21,15 @@ class MakeVideoPage extends StatefulWidget {
 class _MakeVideoPageState extends State<MakeVideoPage> {
   VideoPlayerController vPC;
   ChewieController cC;
+  StreamSubscription subscription;
 
-  String m;
+  String videoFilename;
   var isVideoCompiling = true, framesProcessedCount = 0;
 
   @override
   void initState() {
     super.initState();
-    m = '${widget.appFilesDirectory}/v.mp4';
+    videoFilename = '${widget.appFilesDirectory}/video.mp4';
     compileVideo();
   }
 
@@ -38,29 +40,68 @@ class _MakeVideoPageState extends State<MakeVideoPage> {
     super.dispose();
   }
 
-  void compileVideo() async {
-    var ffmpeg = FlutterFFmpeg();
-    var regex = RegExp(r"frame=[ ]{3}(\d+)");
-    ffmpeg.enableLogCallback((level, message) => setState(() {
-          framesProcessedCount =
-              int.tryParse(regex.firstMatch(message)?.group(1) ?? '') ??
-                  framesProcessedCount;
-        }));
-    await ffmpeg.execute(
-        '-y -i ${widget.photoFilesDirectory}/%05d.jpg -vf zoompan=d=2:fps=1,framerate=5:interp_start=0:interp_end=255:scene=100 $m');
-    vPC = VideoPlayerController.file(File(m));
-    cC = ChewieController(
-        videoPlayerController: vPC,
-        autoPlay: true,
-        looping: true,
-        aspectRatio: widget.size.width / widget.size.height);
-    setState(() {
-      isVideoCompiling = false;
-    });
+  Stream<int> launchCompileVideoProcess() {
+    var sc = StreamController<int>();
+    FlutterFFmpeg ffmpeg;
+    sc.onListen = () async {
+      try {
+        ffmpeg = FlutterFFmpeg();
+        var regex = RegExp(r"frame=[ ]{3}(\d+)");
+        int parseCurrentFrame(String message) =>
+            int.tryParse(regex.firstMatch(message)?.group(1) ?? '');
+
+        ffmpeg.enableLogCallback((level, message) {
+          var frame = parseCurrentFrame(message);
+          if (frame != null) {
+            sc.add(frame);
+          }
+        });
+        var imageFilenameTemplate = '${widget.photoFilesDirectory}/%05d.jpg';
+        var zoompanFilter =
+            '-vf zoompan=d=2:fps=1,framerate=5:interp_start=0:interp_end=255:scene=100';
+        await ffmpeg.execute(
+            '-y -i $imageFilenameTemplate $zoompanFilter $videoFilename');
+      } catch (ex) {
+        sc.addError(ex);
+      } finally {
+        sc.close();
+      }
+    };
+    sc.onCancel = () async {
+      try {
+        await ffmpeg.cancel();
+      } catch (ex) {
+        sc.addError(ex);
+      }
+    };
+
+    return sc.stream;
+  }
+
+  void compileVideo() {
+    newData(int progress) {
+      setState(() {
+        framesProcessedCount = progress;
+      });
+    }
+
+    onDone() {
+      vPC = VideoPlayerController.file(File(videoFilename));
+      cC = ChewieController(
+          videoPlayerController: vPC,
+          autoPlay: true,
+          looping: true,
+          aspectRatio: widget.size.width / widget.size.height);
+      setState(() {
+        isVideoCompiling = false;
+      });
+    }
+
+    subscription = launchCompileVideoProcess().listen(newData, onDone: onDone);
   }
 
   void share() async {
-    await ShareExtend.share(m, "video");
+    await ShareExtend.share(videoFilename, "video");
     Navigator.of(context).pop();
   }
 
@@ -70,8 +111,8 @@ class _MakeVideoPageState extends State<MakeVideoPage> {
     if (!isVideoCompiling) {
       actions.add(IconButton(icon: Icon(Icons.share), onPressed: share));
     }
-    var percentCompleted =
-        (framesProcessedCount * 10 / widget.imageCount).round();
+    var ratio = framesProcessedCount / widget.imageCount;
+    var percentCompleted = (ratio / 100).toStringAsFixed(2);
     var body = Center(
         child: isVideoCompiling
             ? Text('$percentCompleted%')
